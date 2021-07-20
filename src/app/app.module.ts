0b6d915a53c24891e6ba62ff34cfce8b3896273e
Максим Голяков <zigzariz@mail.ru>
2021-07-20 14:15:37 +0300

    Add and styled components in BlogPage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BlogPageComponent } from './pages/blog-page/blog-page.component';
+import { PostListComponent } from './components/post-list/post-list.component';
+import { PostListItemComponent } from './components/post-list-item/post-list-item.component';
+import { MyButtonComponent } from './components/UI/my-button/my-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BlogPageComponent
+    BlogPageComponent,
+    PostListComponent,
+    PostListItemComponent,
+    MyButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/UI/my-button/my-button.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-my-button',
+  templateUrl: './my-button.component.html',
+  styleUrls: ['./my-button.component.scss']
+})
+export class MyButtonComponent {
+
+  @Input() text = ''
+
+}
