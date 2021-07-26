a821a576c70a4908d4fe23292d4887d02f819a41
Максим Голяков <zigzariz@mail.ru>
2021-07-26 22:41:13 +0300

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { BlogPageComponent } from './pages/blog-page/blog-page.component';
+import { CreatePageComponent } from './pages/create-page/create-page.component';
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: BlogPageComponent},
+  {path: 'create', component: CreatePageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { BlogPageComponent } from './pages/blog-page/blog-page.component';
 import { PostListComponent } from './components/post-list/post-list.component';
