5c76a4f3aeaf0bbafe2bfeb291ceeb1e58a7fd29
Максим Голяков <zigzariz@mail.ru>
2021-07-27 03:39:38 +0300

    Create post service with add and get methods

--- src/app/components/post-list/post-list.component.ts
+++ src/app/components/post-list/post-list.component.ts
@@ -1,25 +1,22 @@
+import { PostService } from './../../post.service';
 import { Post } from './../../app.component';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
-  styleUrls: ['./post-list.component.scss']
+  styleUrls: ['./post-list.component.scss'],
+  providers: [PostService]
 })
 export class PostListComponent implements OnInit {
 
   
-  posts: Post[] = [
-    {title: 'title 1', content: 'content 1'},
-    {title: 'title 2', content: 'content 2'},
-    {title: 'title 3', content: 'content 3'},
-    {title: 'title 4', content: 'content 4'},
-    {title: 'title 5', content: 'content 5'},
-  ];
+  posts: Post[] = [];
 
-  constructor() { }
+  constructor(private postService: PostService) { }
 
   ngOnInit(): void {
+    this.posts = this.postService.getPost();
   }
 
 }
---
+++ src/app/post.service.ts
@@ -0,0 +1,22 @@
+import { Post } from './app.component';
+
+
+export class PostService{
+  
+    private posts: Post[] = [
+        {id: 1, title: 'title 1', content: 'content 1'},
+        {id: 2, title: 'title 2', content: 'content 2'},
+        {id: 3, title: 'title 3', content: 'content 3'},
+        {id: 4, title: 'title 4', content: 'content 4'},
+        {id: 5, title: 'title 5', content: 'content 5'},
+    ];
+    
+    getPost(): Post[] {
+          
+        return this.posts;
+    }
+    addPost(post: Post){
+          
+        this.posts.push(post);
+    }
+}
